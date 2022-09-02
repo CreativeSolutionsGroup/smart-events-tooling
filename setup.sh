591a37308ffc896b2c31f8e93702f681770697ee
spbills <spencebills0@gmail.com>
2022-09-02 14:23:00 -0400

    Relative git clone

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ echo "Assuming there is no git key installed on GitHub."
 echo
 echo
 
-git clone https://github.com/CreativeSolutionsGroup/smart-events-terminal-app.git app
+git clone https://github.com/CreativeSolutionsGroup/smart-events-terminal-app.git ../app
 
 echo "WARNING---- This script will now install all dependencies from apt."
 echo "Waiting for five seconds."
