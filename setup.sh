0a6d92d8db32a418e2464bacb818aa7d0cd60d41
spbills <spencebills0@gmail.com>
2022-10-10 14:45:40 -0400

    Added network update script

--- setup.sh
+++ setup.sh
@@ -33,42 +33,7 @@ ExecStart=
 ExecStart=-/sbin/agetty -a kiosk --noclear %I $TERM
 EOF
 
-echo "Enter password for CreativeSolutions email."
-
-read cs_password
-
-sudo cat /etc/NetworkManager/system-connections/eduroam.nmconnection << EOF
-[connection]
-id=eduroam
-uuid=6b1d86bc-aaaf-468f-a134-eb6dad0c693a
-type=wifi
-interface-name=wlp1s0
-
-[wifi]
-mode=infrastructure
-ssid=eduroam
-
-[wifi-security]
-auth-alg=open
-key-mgmt=wpa-eap
-
-[802-1x]
-eap=peap;
-identity=creativesolutions@cedarville.edu
-password=$cs_password
-phase2-auth=mschapv2
-
-[ipv4]
-method=auto
-
-[ipv6]
-addr-gen-mode=default
-method=auto
-
-[proxy]
-EOF
-
-sudo chmod 600 /etc/NetworkManager/system-connections/eduroam.nmconnection
+. network-update.sh
 
 cd ../app
 
