4211c3666efce152f12d808c422719b53e31dab6
spbills <spencebills0@gmail.com>
2022-12-05 15:49:41 -0500

    Removed crontab import

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ echo "WARNING---- This script will now install all dependencies from apt."
 echo "Waiting for five seconds."
 sleep 5
 
-(crontab -l ; echo "* * * * * git -C /home/kiosk/smart-events-terminal-app pull") | crontab -
+#(crontab -l ; echo "* * * * * git -C /home/kiosk/smart-events-terminal-app pull") | crontab -
 
 sudo apt update -y && sudo apt upgrade -y
 
